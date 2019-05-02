9efdcf3a806e6a5ac3dd16f8948f812f72cb7a21
Hourani <kmhouran@CougarNet.UH.EDU>
2019-05-02 14:46:32 -0500

    Factorial Function

--- factorial.py
+++ factorial.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python3
 
-from math import factorial
+def factorial(n, cache=[1]):
+    if n < len(cache) and cache[n]:
+        return cache[n]
+    else:
+        cache.append(n * factorial(n - 1))
+        return cache[n]
 
-def tau(n, cache={}):
-    if n in cache:
+
+def tau(n, cache=[0]):
+    if n < len(cache) and cache[n]:
         return cache[n]
     else:
         i = 2 * n
-        while (n ** i < factorial(i)):
+        while (n ** i >= factorial(i)):
             i += 1
-        cache[n] = i
+        cache.append(i)
         return i
 
+
 def main():
-    for i in range(1, 1000):
-        print(i, tau(i))
+    i = 1
+    while True:
+        print(i, tau(i) / i)
+        i += 1
     return 0
 
 if __name__ == "__main__":
